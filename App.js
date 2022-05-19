41e3f83026755226da94aaa4942f04010f7aa987
Decagon <decagon@Ekene.local>
2022-05-19 01:35:16 +0100

    Referral_Screen

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import OrganizerApp from "./OrganizerApp";
+import Referral from "./ReferralScreen";
 import ChangePassword from "./src/screens/consumer-screens/ConsumerDetails/changePassword";
 export default function App() {
-  return <ChangePassword />;
+  return(
+    <Referral/>
+  )
+  // return <ChangePassword />;
 }
 
 //<OrganizerApp />

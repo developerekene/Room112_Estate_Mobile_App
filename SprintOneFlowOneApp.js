e3a3daba44344bbde74da74317a8076d1b476a1f
Audu-Daniels <aududaniels@gmail.com>
2022-05-20 00:08:06 +0100

    Changes

--- SprintOneFlowOneApp.js
+++ SprintOneFlowOneApp.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useFonts, Manrope_400Regular, Manrope_600SemiBold, Manrope_700Bold, Manrope_300Light, Manrope_500Medium } from '@expo-google-fonts/manrope';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  useFonts,
+  Manrope_400Regular,
+  Manrope_600SemiBold,
+  Manrope_700Bold,
+  Manrope_300Light,
+  Manrope_500Medium,
+} from "@expo-google-fonts/manrope";
 
-import Supplier from './src/screens/supplier-screens/Supplier';
-import Customer from './src/screens/supplier-screens/Customer';
+import Supplier from "./src/screens/supplier-screens/Supplier";
+import Customer from "./src/screens/supplier-screens/Customer";
 import FavouriteCustomers from "./src/screens/supplier-screens/FavouriteCustomers";
 import TotalSupplyMadeActivityOverview from "./src/screens/TotalSupplyMadeActivityOverview";
+// import SupplierScreen from "./src/screens/SupplierScreen/SupplierScreen";
 
 export default function OrganizerApp() {
   let [fontsLoaded] = useFonts({
@@ -14,22 +22,22 @@ export default function OrganizerApp() {
     Manrope_700Bold,
     Manrope_300Light,
     Manrope_600SemiBold,
-    Manrope_500Medium
+    Manrope_500Medium,
   });
 
   if (!fontsLoaded) {
-      return (
-          <View
-              style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Text>loading...</Text>
-          </View>
-      );
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>loading...</Text>
+      </View>
+    );
   }
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto"/>
-      <SafeAreaProvider style={{width: '100%', height: '100%'}}>
+      <StatusBar style="auto" />
+      <SafeAreaProvider style={{ width: "100%", height: "100%" }}>
+        {/* <SupplierScreen /> */}
         <Supplier />
         {/* <Customer /> */}
         {/* <FavouriteCustomers /> */}
@@ -42,8 +50,8 @@ export default function OrganizerApp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

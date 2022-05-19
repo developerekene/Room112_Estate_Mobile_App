0ab983d8848be39c1db50f2b60039f44066f8524
Oluwasegun <adeolusegun99@gmail.com>
2022-05-19 08:35:32 +0100

    brought all the components togther

--- src/screens/consumer-screens/productDetails/buyNow.js
+++ src/screens/consumer-screens/productDetails/buyNow.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native";
 import React from "react";
 import ConsumerHeader from "../../../components/ConsumerHeader/ConsumerHeader";
 import Carousel from "./Carousel";
+import Counter from "./Counter";
 
 const BuyNow = () => {
   return (
@@ -9,6 +10,22 @@ const BuyNow = () => {
       <View style={styles.container}>
         <ConsumerHeader title="Details" />
         <Carousel />
+        <View style={styles.productDetails}>
+          <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+            Bottle Water Pack
+          </Text>
+          <Text style={{ fontWeight: "bold", fontSize: 24 }}>N900.00</Text>
+        </View>
+        <View>
+          <Text>In Stock</Text>
+        </View>
+        <View>
+          <Counter />
+        </View>
+        <Pressable style={styles.btnprimary}>
+          <Text style={styles.btnPrimaryText}>Buy Now</Text>
+        </Pressable>
+        <Text>Remove</Text>
       </View>
     </SafeAreaView>
   );
@@ -21,5 +38,21 @@ const styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
+  },
+  btnprimary: {
+    width: 329,
+    height: 50,
+    backgroundColor: "#147DF5",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  btnPrimaryText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "left",
+    color: "#fff",
+    paddingLeft: 5,
   },
 });

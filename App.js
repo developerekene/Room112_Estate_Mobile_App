fe71abcbd42d58cab9e3f140a1d9fb4b0611d315
Decagon <decagon@Ekene.local>
2022-05-21 02:06:58 +0100

    Reminder Screen

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StyleSheet } from 'react-native';
+import ReminderSent from './ReminderSent';
 import SprintOneFlowOneApp from "./SprintOneFlowOneApp";
 
 export default function OrganizerApp() {
   return (
-    <SprintOneFlowOneApp />
+    // <SprintOneFlowOneApp />
+    <ReminderSent/>
   );
 }
 
-const styles = StyleSheet.create({
-  screenTitle: {
-    paddingTop: 20,
-    paddingBottom: 20,
-    fontSize: 24,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-  },
-});
+// const styles = StyleSheet.create({
+//   screenTitle: {
+//     paddingTop: 20,
+//     paddingBottom: 20,
+//     fontSize: 24,
+//   },
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#FFFFFF",
+//   },
+// });

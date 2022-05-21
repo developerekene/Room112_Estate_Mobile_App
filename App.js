5896bf3e8b22b1f40647d6408426bcf494a52167
Decagon <decagon@Ekene.local>
2022-05-21 03:39:03 +0100

    Add Reminder Screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StyleSheet } from 'react-native';
+import AddReminder from './AddReminder';
 import ReminderSent from './ReminderSent';
 import SprintOneFlowOneApp from "./SprintOneFlowOneApp";
 
 export default function OrganizerApp() {
   return (
     // <SprintOneFlowOneApp />
-    <ReminderSent/>
+    <AddReminder/>
   );
 }
 

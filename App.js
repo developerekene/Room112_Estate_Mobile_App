9e18864e7baebab4d9b878e3b182982a3c62fcfe
Oluwasegun <adeolusegun99@gmail.com>
2022-05-25 09:06:23 +0100

    completed the supplier details screen

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import OrganizerApp from "./OrganizerApp";
-import ChangePassword from "./src/screens/consumer-screens/ConsumerDetails/changePassword";
-import BuyNow from "./src/screens/consumer-screens/productDetails/BuyNow";
+// import OrganizerApp from "./OrganizerApp";
+// import ChangePassword from "./src/screens/consumer-screens/ConsumerDetails/changePassword";
+// import BuyNow from "./src/screens/consumer-screens/productDetails/BuyNow";
 import { StyleSheet } from "react-native";
+//import SuppliersDetailsHeader from "./src/screens/consumer-screens/suppliersdetails/SuppliersDetailsHeader";
+import SupplierDetailsScreen from "./src/screens/consumer-screens/suppliersdetails/SupplierDetailsScreen";
+
 //
 export default function App() {
-  return <BuyNow />;
+  return <SupplierDetailsScreen />;
+
+  //<BuyNow />;
 }
 //<ChangePassword />;
 //import { StyleSheet } from "react-native";
 import AddReminder from "./AddReminder";
 import ReminderSent from "./ReminderSent";
 import AppFlow from "./AppFlow";
+import Header from "./src/screens/consumer-screens/suppliersdetails/SuppliersDetailsHeader";
 
 // export default function OrganizerApp() {
 //   return (

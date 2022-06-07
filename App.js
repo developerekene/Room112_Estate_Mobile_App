f953b336588f4ab3e36df5db93271b644ab275d9
Oluwasegun <adeolusegun99@gmail.com>
2022-06-07 08:48:26 +0100

    api works

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AppFlow from './AppFlow';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppFlow from "./AppFlow";
+import ForgotPasswordScreen from "./src/screens/ForgotPasswordScreen";
+import CreateNewPasswordScreen from "./src/screens/CreateNewPasswordScreen";
 
 export default function App() {
   return (
--- src/screens/ForgotPasswordScreen/ForgotPasswordScreen.js
+++ src/screens/ForgotPasswordScreen/ForgotPasswordScreen.js
@@ -1,51 +1,150 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput, TouchableHighlight } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Input,
+  TouchableHighlight,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
+import SupplierHeader from "../../components/SupplierHeader/SupplierHeader";
+import { Formik } from "formik";
+import { AntDesign } from "@expo/vector-icons";
+import * as yup from "yup";
+
+const validateSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("⚠️ Invalid Email")
+    .required("⚠️ Email is required"),
+});
 
 const ForgotPasswordScreen = ({ navigation }) => {
+  // const [isValidEmail, setIsValidEmail] = useState(false);
   return (
-    <View style={{ paddingHorizontal: 10, paddingTop: 30, backgroundColor: "#FFF", flex: 1 }}>
-      <View>
-        <Text style={{ fontFamily: "Manrope_700Bold", fontSize: 20 }}>Reset Password</Text>
-        <Text style={{ fontFamily: "Manrope_400Regular", fontSize: 14 }}>Enter the email associated with your account and we'll send an email with instruction to reset your password</Text>
-      </View>
-
-      <View style={{ marginTop: 10 }}>
-        <Text
-          style={{
-            fontSize: 14,
-            fontFamily: "Manrope_400Regular",
-            marginVertical: 5,
-          }}
-        >
-          Email
-        </Text>
-        <TextInput
-          placeholder="Email"
-          color="#000"
-          style={{
-            height: 48,
-            borderWidth: 0.5,
-            borderColor: "rgba(33, 51, 79, 0.2)",
-            padding: 10,
-            fontFamily: "Manrope_400Regular",
-          }}
-          value="abayomitunde@gmail.com"
-        />
-      </View>
-
-      <TouchableHighlight style={{marginTop: 20}} underlayColor="#114E93" onPress={() => {
-        navigation.navigate("CheckMail")
-      }} >
-        <View style={{
-          backgroundColor: "#147DF5",
-          height: 48,
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-          <Text style={{color: "#FFF", fontFamily: "Manrope_700Bold"}}>Reset Password</Text>
-        </View>
-      </TouchableHighlight>
-    </View>
+    <Formik
+      initialValues={{ email: "" }}
+      validateOnMount={true}
+      onSubmit={(values) => console.log(values)}
+      validationSchema={validateSchema}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => (
+        <SafeAreaView>
+          <SupplierHeader />
+          <View
+            style={{
+              paddingHorizontal: 10,
+              paddingVertical: 10,
+              backgroundColor: "#FFF",
+            }}
+          >
+            <View>
+              <Text style={{ fontFamily: "Manrope_700Bold", fontSize: 20 }}>
+                Reset Password
+              </Text>
+              <Text
+                style={{
+                  fontFamily: "Manrope_400Regular",
+                  fontSize: 14,
+                  paddingVertical: 10,
+                }}
+              >
+                Enter the email associated with your account and we'll send an
+                email with instruction to reset your password
+              </Text>
+            </View>
+            <View style={{ marginTop: 10 }}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontFamily: "Manrope_400Regular",
+                    marginVertical: 5,
+                  }}
+                >
+                  Email
+                </Text>
+                <View>
+                  <TextInput
+                    placeholder="abayomitunde@gmail.com"
+                    color="#000"
+                    keyboardType="email-address"
+                    value={values.email}
+                    onChangeText={handleChange("email")}
+                    onBlur={handleBlur("email")}
+                    style={{
+                      height: 48,
+                      borderWidth: 0.5,
+                      borderColor: "rgba(33, 51, 79, 0.2)",
+                      padding: 10,
+                      fontFamily: "Manrope_400Regular",
+                      paddingVertical: 10,
+                    }}
+                  />
+                  <AntDesign
+                    style={{
+                      position: "absolute",
+                      top: "25%",
+                      left: "90%",
+                    }}
+                    name={isValid ? "check" : "close"}
+                    size={20}
+                    color={!isValid ? "red" : "green"}
+                  />
+                </View>
+                {errors.email && touched.email && (
+                  <Text
+                    style={{
+                      color: "red",
+                      fontSize: 14,
+                      fontWeight: "bold",
+                      padding: 5,
+                      marginTop: 3,
+                      opacity: 0.4,
+                    }}
+                  >
+                    {errors.email}
+                  </Text>
+                )}
+              </View>
+              <TouchableHighlight
+                style={{ marginTop: 20, paddingVertical: 10 }}
+                underlayColor="#114E93"
+                disabled={!isValid}
+                onPress={() => {
+                  null;
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: isValid ? "#147DF5" : "lightgrey",
+                    height: 48,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{ color: "#FFF", fontFamily: "Manrope_700Bold" }}
+                  >
+                    Reset Password
+                  </Text>
+                </View>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </SafeAreaView>
+      )}
+    </Formik>
   );
 };
 

7239ab1bb7d67dd50e343e5bd52040a8e53346f8
Decagon <decagon@Macs-MacBook-Pro-9.local>
2022-06-08 13:06:46 +0100

    first

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AppFlow from './AppFlow';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppFlow from "./AppFlow";
+import Login from "./Login";
 
 export default function App() {
   return (
     <>
-      <AppFlow />
+      <Login />
     </>
   );
 }
